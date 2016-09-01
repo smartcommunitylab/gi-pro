4c1f76126ab8a0ee9b39d606279e6fe53b766bf1
oskarnrk <o.zambotti@fbk.eu>
2016-09-01 18:00:36 +0200

    Skeleton almost done

--- gi-pro-mobile/www/js/controllers/MainCtrl.js
+++ gi-pro-mobile/www/js/controllers/MainCtrl.js
@@ -4,13 +4,22 @@ angular.module('toga.controllers.main', [])
  * App generic controller
  */
 .controller('AppCtrl', function ($scope, $state, $ionicHistory) {
-	$scope.goTo = function (state, params, root) {
-		if (!!root) {
-			$ionicHistory.nextViewOptions({
-				historyRoot: true
-			});
+	$scope.goTo = function (state, params, disableAnimate, disableBack, historyRoot) {
+		var options = {};
+
+		if (disableAnimate) {
+			options.disableAnimate = disableAnimate;
 		}
 
+		if (disableBack) {
+			options.disableBack = disableBack;
+		}
+
+		if (historyRoot) {
+			options.historyRoot = historyRoot;
+		}
+
+		$ionicHistory.nextViewOptions(options);
 		$state.go(state, params);
 	};
 });
