9821eb389c3f687cd4dd087841fe918e25c8a5a7
oskarnrk <o.zambotti@fbk.eu>
2016-09-06 15:00:47 +0200

    Working on the forms

--- gi-pro-mobile/www/js/controllers/HomeCtrl.js
+++ gi-pro-mobile/www/js/controllers/HomeCtrl.js
@@ -39,7 +39,44 @@ angular.module('toga.controllers.home', [])
 	};
 })
 
-.controller('NewOfferCtrl', function ($scope) {})
+.controller('NewOfferCtrl', function ($scope, $filter, ionicDatePicker, ionicTimePicker) {
+	$scope.newOffer = {
+		poi: null
+	};
+
+	var getSelectedPoi = function (poi) {
+		console.log(poi);
+		$scope.newOffer.poi = poi;
+	};
+
+	$scope.openPoisModal = function () {
+		$scope.$parent.openPoisModal(getSelectedPoi);
+	};
+
+	var datePickerCfg = {
+		setLabel: $filter('translate')('set'),
+		todayLabel: $filter('translate')('today'),
+		closeLabel: $filter('translate')('close'),
+		callback: function (val) {
+			$scope.newOffer.date = val;
+		}
+	};
+	$scope.openDatePicker = function () {
+		ionicDatePicker.openDatePicker(datePickerCfg);
+	};
+
+	var timePickerCfg = {
+		setLabel: $filter('translate')('set'),
+		closeLabel: $filter('translate')('close'),
+		callback: function (val) {
+			console.log(moment(val));
+			$scope.newOffer.time = val;
+		}
+	};
+	$scope.openTimePicker = function () {
+		ionicTimePicker.openTimePicker(timePickerCfg);
+	};
+})
 
 .controller('NotificationsCtrl', function ($scope) {})
 
