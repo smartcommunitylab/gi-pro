2f360fecfce5b0abf44e80c24ecfc79b1c449813
micnori <micnori@yahoo.com>
2016-09-22 13:49:20 +0200

    web: todo and cnf url prop

--- gi-pro-web/src/main/java/it/smartcommunitylab/gipro/integration/CNF.java
+++ gi-pro-web/src/main/java/it/smartcommunitylab/gipro/integration/CNF.java
@@ -100,6 +100,7 @@ public class CNF {
 				professional.setName(getName(nominativo));
 				professional.setSurname(getSurname(nominativo));
 				professional.setAddress(address);
+				//TODO use cfCnf
 				professional.setCf(cf);
 				professional.setType(qualifica.trim());
 				professional.getCustomProperties().put(Const.LawyerDataNascita, dataNascita);
