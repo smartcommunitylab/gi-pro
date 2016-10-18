16cdd39f6d321f0e62f2cae5dba5c34ede01ee6b
kazhamiakin <kazhamiakin@gmail.com>
2016-10-18 16:44:10 +0200

    error type

--- gi-pro-web/src/main/java/it/smartcommunitylab/gipro/common/Utils.java
+++ gi-pro-web/src/main/java/it/smartcommunitylab/gipro/common/Utils.java
@@ -159,7 +159,7 @@ public class Utils {
 
 	public static Map<String,String> handleError(Exception exception) {
 		Map<String,String> errorMap = new HashMap<String,String>();
-		errorMap.put(Const.ERRORTYPE, exception.getClass().toString());
+		errorMap.put(Const.ERRORTYPE, exception.getClass().getSimpleName());
 		errorMap.put(Const.ERRORMSG, exception.getMessage());
 		return errorMap;
 	}
