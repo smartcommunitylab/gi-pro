a04404d4d04e5a15138458e888050d9bffe29f63
chin8 <matteo.chini@fbk.eu>
2016-10-31 17:30:23 +0100

    working on filters

--- gi-pro-mobile/www/js/controllers/ServiceAndProfCtrl.js
+++ gi-pro-mobile/www/js/controllers/ServiceAndProfCtrl.js
@@ -2,7 +2,27 @@ angular.module('gi-pro.controllers.serviceandprof', [])
 
 .controller('ServiceAndProfCtrl', function ($scope, $rootScope, $stateParams, DataSrv, Utils, Login) {
     $scope.services = null;
-    $scope.professionists = null;
+    $scope.professionals = null;
+    $scope.zones = null;
+    $scope.selectingFilter = false;
+    $scope.selectingServices = false;
+    $scope.selectingZones = false;
+    $scope.selectingProfession = false;
+
+    $scope.filters = {
+        selectedService: {
+            id: "1",
+            name: "a"
+        },
+        selectedProfession: {
+            id: "2",
+            name: "b"
+        },
+        selectedZone: {
+            id: "3",
+            name: "c"
+        }
+    };
 
     var reload = function () {
         if (!Login.userIsLogged()) {
@@ -11,19 +31,26 @@ angular.module('gi-pro.controllers.serviceandprof', [])
 
         Utils.loading();
         //get Professionist
-        DataSrv.getProfessionists().then(
-            function (professtionists) {
-                $scope.professionists = professtionists;
-                if (Login.userIsLogged()) {
-                    DataSrv.getServices().then(
-                        function (services) {
-                            $scope.services = services;
-                            Utils.loaded();
-                        },
-                        Utils.commError);
-                } else {
-                    Utils.loaded();
+        DataSrv.getProfessionals().then(
+            function (professionals) {
+                if (professionals) {
+                    $scope.professionals = professionals;
                 }
+                //                get zones
+                DataSrv.getZones().then(function (zones) {
+                        $scope.zones = zones;
+                        if (Login.userIsLogged()) {
+                            DataSrv.getServices().then(
+                                function (services) {
+                                    $scope.services = services;
+                                    Utils.loaded();
+                                },
+                                Utils.commError);
+                        } else {
+                            Utils.loaded();
+                        }
+                    },
+                    Utils.commError);
             },
             Utils.commError);
         //if Login.userIsLogged() load services
@@ -34,7 +61,31 @@ angular.module('gi-pro.controllers.serviceandprof', [])
         // prevent double load (WHY?!?!?)
         reload();
     }
+    $scope.openFilters = function (type) {
+        $scope.selectingFilter = true;
+        if (type === 'service') {
+            $scope.selectingServices = true;
+        }
+        if (type === 'zone') {
+            $scope.selectingZones = true;
+        }
+        if (type === 'profession') {
+            $scope.selectingProfession = true;
+        }
+    }
 
+    $scope.selectFilter = function (type, selection) {
+        $scope.selectingFilter = false;
+        $scope.selectingServices = false;
+        $scope.selectingZones = false;
+        $scope.selectingProfession = false;
+
+        if (type === 'service') {
+            //            $scope.filters.selectedService =
+        }
+        if (type === 'zone') {}
+        if (type === 'profession') {}
+    }
     $scope.$on('$ionicView.enter', function (event, args) {
         var params = DataSrv.internalCache['app.serviceAndProf'] || {};
         if (!!params.reload) {
