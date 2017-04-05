5842fb93767217ba6fe61296603b9ce3c9897987
chin8 <matteo.chini@fbk.eu>
2017-04-05 16:40:04 +0200

    related to previous commit

--- gi-pro-mobile/www/js/app.js
+++ gi-pro-mobile/www/js/app.js
@@ -28,345 +28,346 @@ angular.module('gi-pro', [
   'gi-pro.directives'
 ])
 
-  .run(function ($ionicPlatform, $rootScope, Login, PushSrv) {
-    $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
-        cordova.plugins.Keyboard.disableScroll(true)
-      }
-
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault()
-      }
-    })
-
-    if (Login.getUser()) {
-      console.log('user logged: ' + Login.getUser().objectId + ' (' + Login.getUser().name + ' ' + Login.getUser().surname + ')')
-      PushSrv.init(Login.getUser().objectId)
+.run(function ($ionicPlatform, $rootScope, Login, PushSrv) {
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true)
+      cordova.plugins.Keyboard.disableScroll(true)
     }
 
-    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
-      PushSrv.fgOf()
-    })
-  })
-
-  .config(function ($ionicConfigProvider, $httpProvider, $translateProvider) {
-    $httpProvider.defaults.withCredentials = true
-
-    // PROBLEM WITH SCROLL RESIZE ON OLD ANDROID DEVICES
-    $ionicConfigProvider.scrolling.jsScrolling(ionic.Platform.isIOS() || (ionic.Platform.isAndroid() && parseFloat(ionic.Platform.version()) < 4.4))
-
-    $ionicConfigProvider.tabs.position('top')
-    $ionicConfigProvider.tabs.style('striped')
-    $ionicConfigProvider.backButton.previousTitleText(false).text('')
-    $ionicConfigProvider.navBar.alignTitle('left')
-
-    // $ionicConfigProvider.navBar.alignTitle('left');
-
-    // $translateProvider.translations('it', {});
-    $translateProvider.preferredLanguage('it')
-    $translateProvider.useStaticFilesLoader({
-      prefix: 'languages/',
-      suffix: '.json'
-    })
-
-    // $translateProvider.useSanitizeValueStrategy('sanitize');
-    // $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
-    $translateProvider.useSanitizeValueStrategy('escapeParameters')
-  })
-
-  .config(function (ionicDatePickerProvider, ionicTimePickerProvider) {
-    // FIXME language fixed to 'it' dev only!
-    moment.locale('it')
-
-    var datePickerObj = {
-      inputDate: new Date(),
-      mondayFirst: true,
-      weeksList: moment.weekdaysMin(),
-      monthsList: moment.monthsShort(),
-      templateType: 'popup',
-      from: new Date(),
-      showTodayButton: true,
-      dateFormat: 'dd MMMM yyyy'
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault()
     }
-    ionicDatePickerProvider.configDatePicker(datePickerObj)
-
-    var timePickerObj = {
-      inputTime: (((new Date()).getHours() * 60 * 60) + ((new Date()).getMinutes() * 60)),
-      format: 24,
-      step: 1
-    }
-    ionicTimePickerProvider.configTimePicker(timePickerObj)
   })
 
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
-      })
+  if (Login.getUser()) {
+    console.log('user logged: ' + Login.getUser().objectId + ' (' + Login.getUser().name + ' ' + Login.getUser().surname + ')')
+    PushSrv.init(Login.getUser().objectId)
+  }
 
-      .state('app.tutorial', {
-        url: '/tutorial',
-        params: {
-          forceReload: false
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/tutorial.html',
-            controller: 'TutorialCtrl'
-          }
-        }
-      })
+  $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
+    PushSrv.fgOf()
+  })
+})
 
-      .state('app.serviceAndProf', {
-        url: '/home',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/serviceAndProf.html',
-            controller: 'ServiceAndProfCtrl'
-          }
-        }
-      })
+.config(function ($ionicConfigProvider, $httpProvider, $translateProvider) {
+  $httpProvider.defaults.withCredentials = true
 
-      .state('app.requests', {
-        url: '/requests',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/requests.html',
-            controller: 'RequestsCtrl'
-          }
-        }
-      })
+  // PROBLEM WITH SCROLL RESIZE ON OLD ANDROID DEVICES
+  $ionicConfigProvider.scrolling.jsScrolling(ionic.Platform.isIOS() || (ionic.Platform.isAndroid() && parseFloat(ionic.Platform.version()) < 4.4))
 
-      .state('app.professionalDetails', {
-        url: '/professional/{objectId}',
-        params: {
-          'objectId': null,
-          'professional': null
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/professionalDetails.html',
-            controller: 'ProfileCtrl'
-          }
-        }
-      })
+  $ionicConfigProvider.tabs.position('top')
+  $ionicConfigProvider.tabs.style('striped')
+  $ionicConfigProvider.backButton.previousTitleText(false).text('')
+  $ionicConfigProvider.navBar.alignTitle('left')
 
-      .state('app.professionalWithServices', {
-        url: '/professional/{objectId}',
-        params: {
-          'objectId': null,
-          'professional': null
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/professionalWithServices.html',
-            controller: 'ProfileCtrl'
-          }
-        }
-      })
+  // $ionicConfigProvider.navBar.alignTitle('left');
 
-      .state('app.servicedetails', {
-        url: '/prof/{objectId}',
-        params: {
-          'service': null,
-          'objectId': null,
-          'professionist': null
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/serviceDetails.html',
-            controller: 'ServiceDetailsCtrl'
-          }
-        }
-      })
+  // $translateProvider.translations('it', {});
+  $translateProvider.preferredLanguage('it')
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'languages/',
+    suffix: '.json'
+  })
 
-      /*
-      .state('app.map', {
-        url: "/map",
-        views: {
-          'menuContent': {
-            templateUrl: "templates/map.html",
-            controller: 'MapCtrl'
-          }
-        }
-      })
-      */
+  // $translateProvider.useSanitizeValueStrategy('sanitize');
+  // $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
+  $translateProvider.useSanitizeValueStrategy('escapeParameters')
+})
 
-      .state('app.requestdetails', {
-        url: '/requests/{objectId}',
-        params: {
-          'objectId': null,
-          'request': null
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/request.html',
-            controller: 'RequestDetailsCtrl'
-          }
-        }
-      })
+.config(function (ionicDatePickerProvider, ionicTimePickerProvider) {
+  // FIXME language fixed to 'it' dev only!
+  moment.locale('it')
 
-      /*
-      .state('app.offerdetails', {
-        url: '/offer/{objectId}',
-        params: {
-          'objectId': null,
-          'offer': null
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/offer.html',
-            controller: 'OfferDetailsCtrl'
-          }
-        }
-      })
-      */
+  var datePickerObj = {
+    inputDate: new Date(),
+    mondayFirst: true,
+    weeksList: moment.weekdaysMin(),
+    monthsList: moment.monthsShort(),
+    templateType: 'popup',
+    from: new Date(),
+    showTodayButton: true,
+    dateFormat: 'dd MMMM yyyy'
+  }
+  ionicDatePickerProvider.configDatePicker(datePickerObj)
 
-      .state('app.search', {
-        url: '/search',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/search.html',
-            controller: 'SearchOffersCtrl'
-          }
-        }
-      })
+  var timePickerObj = {
+    inputTime: (((new Date()).getHours() * 60 * 60) + ((new Date()).getMinutes() * 60)),
+    format: 24,
+    step: 1
+  }
+  ionicTimePickerProvider.configTimePicker(timePickerObj)
+})
 
-      .state('app.searchresults', {
-        url: '/search/results',
-        params: {
-          'results': null
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/searchresults.html',
-            controller: 'SearchOffersResultsCtrl'
-          }
-        }
-      })
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'AppCtrl'
+    })
 
-      /*
-      .state('app.newrequest', {
-        url: '/request/new',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/form_request.html',
-            controller: 'NewRequestCtrl'
-          }
-        }
-      })
-
-      .state('app.newoffer', {
-        url: '/offer/new',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/form_offer.html',
-            controller: 'NewOfferCtrl'
-          }
-        }
-      })
-      */
-
-      .state('app.notifications', {
-        url: '/notifications',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/notifications.html',
-            controller: 'NotificationsCtrl'
-          }
-        }
-      })
-
-      /*
-      .state('app.history', {
-        url: '/history',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/history.html',
-            controller: 'HistoryCtrl'
-          }
-        }
-      })
-      */
-
-      .state('app.profile', {
-        url: '/profile',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/professionalWithServices.html',
-            controller: 'ProfileCtrl'
-          }
-        }
-      })
-
-      .state('app.credits', {
-        url: '/credits',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/credits.html'
-          }
-        }
-      })
-
-      .state('app.login', {
-        cache: false,
-        url: '/login',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/login.html',
-            controller: 'LoginCtrl'
-          }
-        }
-      })
-
-      .state('app.registration1', {
-        cache: false,
-        url: '/registration1',
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/registration_step_1.html',
-            controller: 'RegistrationFirstCtrl'
-          }
-        }
-      })
-
-      .state('app.registration2', {
-        cache: false,
-        url: '/registration2',
-        params: {
-          obj: null
-        },
-        views: {
-          'menuContent': {
-            templateUrl: 'templates/registration_step_2.html',
-            controller: 'RegistrationSecondCtrl'
-          }
-        }
-      })
-
-    // if none of the above states are matched, use this as the fallback$state
-    $urlRouterProvider.otherwise(function ($injector) {
-      var Login = $injector.get('Login')
-      var logged = Login.getUser()
-      if (!logged) {
-        return '/app/tutorial'
-      } else {
-        $injector.get('$rootScope').user = logged
-        Login.updateUser().then(function () {}, function (errCode) {
-          if (errCode === Login.USER_ERRORS.NO_USER) {
-            $injector.get('Utils').loaded()
-            Login.logout()
-            $injector.get('$state').go('app.tutorial', {})
-          }
-        })
+  .state('app.tutorial', {
+    url: '/tutorial',
+    params: {
+      forceReload: false
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/tutorial.html',
+        controller: 'TutorialCtrl'
       }
-      return '/app/home'
-    })
+    }
   })
+
+  .state('app.serviceAndProf', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/serviceAndProf.html',
+        controller: 'ServiceAndProfCtrl'
+      }
+    }
+  })
+
+  .state('app.requests', {
+    url: '/requests',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/requests.html',
+        controller: 'RequestsCtrl'
+      }
+    }
+  })
+
+  .state('app.professionalDetails', {
+    url: '/professional/{objectId}',
+    cache: false,
+    params: {
+      'objectId': null,
+      'professional': null
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/professionalDetails.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('app.professionalWithServices', {
+    url: '/professional/{objectId}',
+    params: {
+      'objectId': null,
+      'professional': null
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/professionalWithServices.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('app.servicedetails', {
+    url: '/prof/{objectId}',
+    params: {
+      'service': null,
+      'objectId': null,
+      'professionist': null
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/serviceDetails.html',
+        controller: 'ServiceDetailsCtrl'
+      }
+    }
+  })
+
+  /*
+  .state('app.map', {
+    url: "/map",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/map.html",
+        controller: 'MapCtrl'
+      }
+    }
+  })
+  */
+
+  .state('app.requestdetails', {
+    url: '/requests/{objectId}',
+    params: {
+      'objectId': null,
+      'request': null
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/request.html',
+        controller: 'RequestDetailsCtrl'
+      }
+    }
+  })
+
+  /*
+  .state('app.offerdetails', {
+    url: '/offer/{objectId}',
+    params: {
+      'objectId': null,
+      'offer': null
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/offer.html',
+        controller: 'OfferDetailsCtrl'
+      }
+    }
+  })
+  */
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html',
+        controller: 'SearchOffersCtrl'
+      }
+    }
+  })
+
+  .state('app.searchresults', {
+    url: '/search/results',
+    params: {
+      'results': null
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/searchresults.html',
+        controller: 'SearchOffersResultsCtrl'
+      }
+    }
+  })
+
+  /*
+  .state('app.newrequest', {
+    url: '/request/new',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/form_request.html',
+        controller: 'NewRequestCtrl'
+      }
+    }
+  })
+
+  .state('app.newoffer', {
+    url: '/offer/new',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/form_offer.html',
+        controller: 'NewOfferCtrl'
+      }
+    }
+  })
+  */
+
+  .state('app.notifications', {
+    url: '/notifications',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/notifications.html',
+        controller: 'NotificationsCtrl'
+      }
+    }
+  })
+
+  /*
+  .state('app.history', {
+    url: '/history',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/history.html',
+        controller: 'HistoryCtrl'
+      }
+    }
+  })
+  */
+
+  .state('app.profile', {
+    url: '/profile',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/professionalWithServices.html',
+        controller: 'ProfileCtrl'
+      }
+    }
+  })
+
+  .state('app.credits', {
+    url: '/credits',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/credits.html'
+      }
+    }
+  })
+
+  .state('app.login', {
+    cache: false,
+    url: '/login',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+      }
+    }
+  })
+
+  .state('app.registration1', {
+    cache: false,
+    url: '/registration1',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/registration_step_1.html',
+        controller: 'RegistrationFirstCtrl'
+      }
+    }
+  })
+
+  .state('app.registration2', {
+    cache: false,
+    url: '/registration2',
+    params: {
+      obj: null
+    },
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/registration_step_2.html',
+        controller: 'RegistrationSecondCtrl'
+      }
+    }
+  })
+
+  // if none of the above states are matched, use this as the fallback$state
+  $urlRouterProvider.otherwise(function ($injector) {
+    var Login = $injector.get('Login')
+    var logged = Login.getUser()
+    if (!logged) {
+      return '/app/tutorial'
+    } else {
+      $injector.get('$rootScope').user = logged
+      Login.updateUser().then(function () {}, function (errCode) {
+        if (errCode === Login.USER_ERRORS.NO_USER) {
+          $injector.get('Utils').loaded()
+          Login.logout()
+          $injector.get('$state').go('app.tutorial', {})
+        }
+      })
+    }
+    return '/app/home'
+  })
+})
